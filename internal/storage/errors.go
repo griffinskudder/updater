8c96b9366499d9d7e7d2cf5700b245850ae5ffe5
Griffin Skudder <griffinskudder@gmail.com>
2026-02-16 21:27:32 +1100

    feat(storage): add ErrNotFound sentinel

--- internal/storage/errors.go
+++ internal/storage/errors.go
@@ -2,5 +2,8 @@ package storage
 
 import "errors"
 
+// ErrNotFound is returned when a requested resource does not exist.
+var ErrNotFound = errors.New("not found")
+
 // ErrHasDependencies is returned when attempting to delete a resource that has dependent records.
 var ErrHasDependencies = errors.New("resource has dependent records")
